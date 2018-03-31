9c4586414743cd18837b2c2560277c588f960a1b
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2018-03-31 11:43:19 +0530

    add User Table/Class which stores id username and password.

--- models.py
+++ models.py
@@ -6,7 +6,8 @@ from passlib.apps import custom_app_context as pwd_context
 
 Base = declarative_base()
 
-
+class User(Base):
+    __tablename__ = 'user'
 
 engine = create_engine('sqlite:///users.db')
 
